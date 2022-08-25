e3c64a210fb3e01c327f34ed750da4fa287ceabc
chaitravb <chaitrabelagavi52@gmail.com>
2022-08-25 17:11:16 +0530

    edited Conditions.java

--- src/main/java/day3/Conditions.java
+++ src/main/java/day3/Conditions.java
@@ -3,7 +3,8 @@ package day3;
 public class Conditions {
     public static void main(String[] args) {
         int age=18;
-        if(age>=18) {
+        if(age>=18)
+        {
             System.out.println(" you can vote");
         }else{
             System.out.println("18+ can only vote");
